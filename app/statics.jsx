f89e27892eaac012732807144df035190a3096d5
LuisC95 <luiscarmona.95@gmail.com>
2024-10-16 15:03:34 -0400

    button added

--- app/statics.jsx
+++ app/statics.jsx
@@ -1,10 +1,10 @@
-import { Text, View } from "react-native";
+import { Button,Text, View } from "react-native";
 
 export default function StaticsButton() 
 {
     return(
         <View>
-        <Text>Statics</Text>
+        <Button title="Statics" onPress={() => console.log("Statics")} /> 
         </View>
     );
 
